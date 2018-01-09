9e7688faaa3cfc392cefa097eeb489fc3eb7b687
isaias.neto <isaias.neto@pcinformatica.com.br>
2018-01-09 16:35:08 -0200

    Aula 09 - Muitos objetos e o Flyweight

--- src/main/java/br/com/alura/dp/flyweight/package-info.java
+++ src/main/java/br/com/alura/dp/flyweight/package-info.java
@@ -2,6 +2,15 @@
  * @author isaias.neto
  * Diferença entre Factory e Flyweight
  * 
+ * Uma Factory instancia uma classe que é importante/complexa, e seu processo de criação deve ser isolado.
  * 
+ * Um Flyweight serve para quando temos muitas instâncias do mesmo objeto andando pelo sistema, 
+ * e precisamos economizar. Para tal, o Flyweight faz uso de uma fábrica modificada, que guarda essas instâncias.
+ * 
+ * Diferença de Flyweight e Singleton
+ * 
+ * A ideia de ambos é garantir que existam apenas uma única referência para o objeto ao longo do programa.
+ * 
+ * A diferença é que o Flyweight garante que existam apenas uma única instância de vários elementos. É um "singleton maior".
  */
 package br.com.alura.dp.flyweight;
